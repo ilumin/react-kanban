b95d6cb8c2287bd5a71dba8639e32a522d8beedd
Teerasak Vichadee <iolumin@gmail.com>
2015-12-14 22:09:32 +0700

    Config "hot module replacement" to render on webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 const PATH = {
   app: path.join(__dirname, 'app'),
@@ -12,7 +13,17 @@ module.exports = {
     path: PATH.build,
     filename: 'bundle.js'
   },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    stats: 'errors-only',
+    host: process.env.HOST,
+    port: process.env.PORT
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       title: 'Kanban App'
     })
