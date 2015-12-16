0ae76a060d74697b68d5c4e5cbe5760b0c1669f5
Teerasak Vichadee <iolumin@gmail.com>
2015-12-16 07:55:59 +0700

    Create Lanes component / action / store

---
+++ app/components/Lanes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Lanes extends React.Component {
+  render() {
+    return (
+      <div clasName="lanes">
+        lanes should go here
+      </div>
+    );
+  }
