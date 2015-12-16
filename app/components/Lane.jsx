f8e82212831a4be2eac91cb7b38c16e5384c46bb
Teerasak Vichadee <iolumin@gmail.com>
2015-12-16 22:46:45 +0700

    Imprement DropTarget on Lane component

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -6,6 +6,22 @@ import NoteStore from '../stores/NoteStore';
 import LaneActions from '../actions/LaneActions';
 import Editable from './Editable.jsx';
 
+import {DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
+
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+    const sourceId = sourceProps.id;
+    if (!targetProps.lane.notes.length) {
+      console.log('source', sourceId, 'target', targetProps);
+    }
+  }
+};
+
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
 export default class Lane extends React.Component {
   constructor(props) {
     super(props);
@@ -19,9 +35,13 @@ export default class Lane extends React.Component {
   }
 
   render() {
-    const {lane, ...props} = this.props;
+    const {
+      connectDropTarget,
+      lane,
+      ...props
+    } = this.props;
 
-    return (
+    return connectDropTarget(
       <div {...props}>
         <div className="lane-header">
           <Editable
