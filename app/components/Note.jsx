ba62884403853de1650838158a31ac40f676e42d
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2015-12-16 18:11:32 +0700

    Imprement drag item

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import {DragSource} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 
+const noteSource = {
+  beginDrag(props) {
+    console.log('begin draggin note', props);
+    return {};
+  }
+};
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
 export default class Note extends React.Component {
   render() {
+    const {connectDragSource, id, onMove, ...props} = this.props;
+
     return (
       <li {...this.props}>{this.props.children}</li>
     );
