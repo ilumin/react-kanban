95995231b2f588e643fa76590b733bb7311acd2e
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2015-12-16 10:59:01 +0700

    Create action on store

--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -1,6 +1,7 @@
 import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import LaneActions from '../actions/LaneActions';
+import NoteStore from './NoteStore';
 
 class LaneStore {
   constructor() {
@@ -15,6 +16,28 @@ class LaneStore {
     this.setState({
       lanes: lanes.concat(lane)
     });
+  }
+
+  attachToLane({laneId, noteId}) {
+    if (!noteId) {
+      this.waitFor(NoteStore);
+    }
+
+    noteId = NoteStore.getState().notes.slice(-1)[0].id;
+
+    const lanes = this.lanes.map((lane) => {
+      if (lane.id === laneId) {
+        if (lane.notes.indexOf(noteId) === -1) {
+          lane.notes.push(noteId);
+        }
+        else {
+          console.warn('Already attached note to lane', lanes);
+        }
+      }
+      return lane;
+    });
+
+    this.setState({lanes});
   }
 }
 
