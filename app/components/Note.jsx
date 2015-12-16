2b7de1c10fe9afa86430d501b781cb8c55e93d6e
Teerasak Vichadee <iolumin@gmail.com>
2015-12-16 22:38:36 +0700

    Imprement isDragging event

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -22,8 +22,9 @@ const noteTarget = {
   }
 };
 
-@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
-  connectDragSource: connect.dragSource()
+@DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
 }))
 @DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
   connectDropTarget: connect.dropTarget()
@@ -33,13 +34,18 @@ export default class Note extends React.Component {
     const {
       connectDragSource,
       connectDropTarget,
+      isDragging,
       id,
       onMove,
       ...props
     } = this.props;
 
     return connectDragSource(connectDropTarget(
-      <li {...this.props}>{this.props.children}</li>
+      <li style={{
+        opacity: isDragging ? 0 : 1
+      }} {...this.props}>
+        {this.props.children}
+      </li>
     ));
   }
 }
